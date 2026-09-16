0a72be856af3e101b3d1bc52156c2cee19fad99b
GiuseppeMarques <www.gfm@gmail.com>
2026-09-16 10:49:35 -0300

    Add files via upload

--- sw-vistoria-campo.js
+++ sw-vistoria-campo.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vistoria-campo-v49';
+const CACHE_NAME = 'vistoria-campo-v50';
 
 self.addEventListener('install', function(e){
   self.skipWaiting();
--- sw-vistoria-quartel.js
+++ sw-vistoria-quartel.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vistoria-quartel-v79';
+const CACHE_NAME = 'vistoria-quartel-v80';
 
 self.addEventListener('install', function(e){
   self.skipWaiting();
